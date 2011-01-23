f4d388c49b7d38e1ceba8967c71c9e82bd94a480
Javier Gonel <bolibic@gmail.com>
2011-01-23 14:33:02 +0200

    It writes to the DB as it should.
    TODO
    - Update sameple documents in couchapp
    - Document format to be used in UDP

--- udpserver.js
+++ udpserver.js
@@ -14,24 +14,40 @@ db.exists(function (message, result) {
 
 function startApp () {
 	var server = udp.createSocket('udp4');
-	server.on("message", function(msg, rinfo) {
-		console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+	server.on("message", processMessage);
+	server.on("listening", function() { 
+		var addr = server.address();	
+		console.log("listening "+addr.port); 
 	});
-	server.on("listening", function() { console.log("listening"); });
-
+	server.on("error",function(ex) {console.log(ex);});
 	server.bind(31337);
 };
 
-function processMessage(msg,rinfo) {
-	console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
-	// Check message
+/*
+	Splits a message into the fundamental parts
+*/
+function processMessage(buffer,rinfo) {
+	var msg = buffer.toString('utf8'),
+		msgForDb = msg.split("\t",6),
+		messagePos = 0;
+		i = 0;
+
+	console.log("Incoming: '" + msg + "' from: " + rinfo.address + ":" + rinfo.port);
+
+	// Check message: 
+  // Format: Application<tab>Environment<tab>Level<tab>Timestamp<tab>Where<tab>Message
+	if (msgForDb.length != 6) { 
+		console.log("Invalid message");
+		return;
+	}
+	
+	// Find the message position
+	for (;i<5;i++)
+		messagePos = msg.indexOf('\t',messagePos+1);
+	msgForDb[5]=msg.substr(messagePos+1);
+
 	// Save it
-	db.save({
-		App:"",
-		Env:"",
-		Level:"",
-		Timestamp:"",
-		Where:"",
-		Msg:""		
-	});
+	// We add the version number and we don't add field names (sorry)
+	db.save({d:[1].concat(msgForDb)});
+	console.log("Saved!");
 }
